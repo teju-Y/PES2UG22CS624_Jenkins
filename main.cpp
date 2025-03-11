07d385c1838391060dee5bf4349cc0864421ae42
teju-Y <pes2ug22cs624@pesu.pes.edu>
2025-03-11 19:40:27 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from Jenkins Pipeline!" << std::endl;
+    std::cout << "This is a test C++ program for CI/CD demonstration." << std::endl;
+    return 0;
+}
